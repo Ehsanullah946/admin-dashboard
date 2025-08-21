0b0db4d1b9c8ae0354c0b4aa5ae58a0398a5e6ae
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-21 22:28:40 +0430

    created orders and employees page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,9 +18,9 @@ const Sidebar = () => {
   };
 
   const activeLink =
-    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-900 text-md m-2';
   const normalLink =
-    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-500 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
     <div className="ml-3 h-screen md:overflow-hidden md:hover:overflow-auto overflow-auto pb-10">
       {activeMenue && (
